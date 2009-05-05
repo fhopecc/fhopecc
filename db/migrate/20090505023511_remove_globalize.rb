bbbd534dc0940ca52c742faa5610ff72e5d0f0e0
fhopecc <fhopecc@gmail.com>
2009-05-05 10:57:57 +0800

    debugs

--- db/migrate/20090505023511_remove_globalize.rb
+++ db/migrate/20090505023511_remove_globalize.rb
@@ -1,8 +1,12 @@
 class RemoveGlobalize < ActiveRecord::Migration
+	def drop_exist_table table
+    drop_table(table) if table_exists?(table)
+	end
+
   def self.up
-    drop_table :globalize_countries
-    drop_table :globalize_languages
-    drop_table :globalize_translations
+    drop_exist_table :globalize_countries
+    drop_exist_table :globalize_languages
+    drop_exist_table :globalize_translations
   end
 
   def self.down
