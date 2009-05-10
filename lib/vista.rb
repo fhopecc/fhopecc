90ae681527e7dfcd32366de0b795103d73315e45
unknown <fhopecc@.(none)>
2009-05-10 22:05:19 +0800

    some work at home

--- lib/vista.rb
+++ lib/vista.rb
@@ -1,15 +1,35 @@
 require 'winxp'
 module VISTA
 	include WINXP
-  def set_sysenv(var, val)
-    unless system "setx #{var} #{val} >nul"
-			puts "error:setx #{var} \"#{val}\""
+  def set_sysenv var, val
+    unless system "setx #{var} \"#{val}\" /M >nul 2>nul"
+			puts "Fails " if var != 'PATH'
 		end
 	end
 
-  def add_path(path)
-		env_paths = ENV['PATH'].split(/;/).push(path).uniq
-		strpath =  env_paths.join(";")
-    set_sysenv 'PATH', strpath
+  def add_path path
+		unless ENV['PATH'].split(/;/).include? path
+      set_sysenv 'PATH', "%PATH%;#{path}"
+			puts "Path [#{path}] pushesin PATH envvar successfully!"
+		else
+			puts "Path [#{path}] is already in PATH envvar!"
+		end
+	end
+
+	def uniq_path
+		upaths = ENV['PATH'].split(/;/).uniq 
+    set_sysenv 'PATH', "#{upaths.join(';')}"
+	  puts "PATH has unique its elements successfully!"
+	end
+
+	def del_path path
+		paths = ENV['PATH'].split(/;/)
+		if paths.include? path
+			paths.delete path
+      set_sysenv 'PATH', "#{paths.join(';')}"
+			puts "Path [#{path}] has removed from PATH envvar successfully!"
+		else
+			puts "Path [#{path}] is not in PATH envvar!"
+		end
 	end
 end
