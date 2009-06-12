59c5285b8935af06856e5082cd494490ec3a27a6
fhopecc <fhopecc@gmail.com>
2009-06-12 17:42:30 +0800

    some thing

--- config/routes.rb
+++ config/routes.rb
@@ -1,75 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-
-  map.resources :users
-
-  map.resources :abouts
-
   map.resource  :session
-
-  map.resources :mlogs, :collection => {:help => :get}
-
-	map.resources :monthly_mlogs
-
-	map.resources :audit_trails
-
+  map.resources :hltbs
   map.resources :users
-
-  map.resources :monthly_taglists
-
-  map.resources :yhq_codes
-
-  map.resource  :db_dumper
-
   map.resources :helpers
-
   map.resources :counters
-
-  map.resources :tag_trees, :collection => {:load_form => :get, :load => :post} do |tag_tree|
-		tag_tree.resources :tag_trees, :controller => 'tag_trees'
-	end
-
-  map.resources :db_checklists
-
-
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  map.root :controller => "monthly_mlogs"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.root      :controller => "hltbs"
+  map.connect   ':controller/:action/:id'
+  map.connect   ':controller/:action/:id.:format'
 end
