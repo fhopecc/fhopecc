85ff97a01bdded135e493132481b99f74deb9df5
unknown <fhopecc@.(none)>
2009-08-12 21:38:11 +0800

    pydoc

--- lib/pydoc.py
+++ lib/pydoc.py
@@ -1,9 +1,20 @@
-def sect(name, *nodes):
-  o = "name is " + name
-  for n in nodes:
-    o = o + n
-  return o
-class DocTreeNode:
+class OpenStruct:
+	def __init__(self, **dic):
+		self.__dict__.update(dic)
+	def __getattr__(self, i):
+		if i in self.__dict__:
+			return self.__dict__[i]
+		else:
+			raise AttributeError, i
+
+	def __setattr__(self,i,v):
+		if i in self.__dict__:
+			self.__dict__[i] = v
+		else:
+			self.__dict__.update({i:v})
+		return v # i like cascates :)
+
+class DocTreeNode(OpenStruct):
   def __init__(self, name, type=None, value=None, parent=None):
     self.name     = name
     self.parent   = parent
@@ -16,9 +27,17 @@ class DocTreeNode:
     self.children.append(c)
     c.parent = self
 
+def sect(name, title, *nodes):
+  return DocTreeNode(name, nodes)
+
 # Test in Windows Path Environment
 import unittest
 class DocTreeNodeTest(unittest.TestCase):
+  def testOpenStruct(self):
+    d = DocTreeNode('1')
+    d.new_attr = "new value"
+    self.assertEqual('new value', d.new_attr)
+    self.assertRaises(AttributeError, lambda :d.no_attr)
   def testSingleNode(self):
     d = DocTreeNode('1')
     self.assertEqual('1', d.name)
@@ -29,12 +48,13 @@ class DocTreeNodeTest(unittest.TestCase):
     d.addChild(c)
     self.assertEqual('2', d.children[0].name)
     self.assertEqual('1', c.parent.name)
-  def testSect(self):
-    o = sect("name", '''abcd
-def rwd
-  class 
-''', "abcd")
-    self.assertEqual('2', o)
+#  def testSect(self):
+    #n = sect("sect name", 
+#"This is a section title", '''
+#This is section content, 
+#and it's multi lines.
+#''')
+#    self.assertEqual('sect name', n.name)
 
 if __name__ == '__main__':
  unittest.main()
